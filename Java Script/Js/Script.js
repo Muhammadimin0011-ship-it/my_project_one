d3036003c513e4b79bdc29d965c3ef6069cabf16
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-02-05 13:50:50 +0500

    Script.js

---
+++ Java Script/Js/Script.js
@@ -0,0 +1,20 @@
+var name = prompt("Ismi").trim();
+var age = prompt("yoshingiz").trim();
+if (age > 18 && age < 28) {
+    var result = prompt("Jinsigiz").trim();
+    if (result == "women") {
+        console.log("Raxmat");
+    } else (result == "men") 
+        var question = prompt("Armiyaga borganmisiz").trim();
+        if (result == "ha") {
+            console.log("Siz armiyaga borishingiz shart emas");
+        } else (result == "yoq") 
+            console.log("Siz armiyaga borasiz");
+        }
+    
+
+ else {
+    console.log("yoq ");
+}
+
+
