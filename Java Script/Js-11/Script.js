f3f501ce13d3da4c35305ec8bd0e4c36f0e990bb
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-02-21 16:18:54 +0500

    Script.js

--- Java Script/Js-11/Script.js
+++ Java Script/Js-11/Script.js
@@ -1,7 +1,13 @@
 var text = prompt("Uzunroq matn yozing (olma,anor,banan)").trim();
 
+
+
 var words = text.split(",");
+
+
+
 
 for (var i = 0; i < words.length; i++) {
     console.log((i + 1) + " 👉 " + words[i]);
 }
+console.log("JAmi sozlar:" + words.length);
