712d4c504b33e8712bb4d633ad33cf7c24e36750
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-03-05 16:05:33 +0500

    Script.js

--- Java Script/Js-15/Script.js
+++ Java Script/Js-15/Script.js
@@ -6,25 +6,26 @@ if (metodar === "length") {
     console.log(arr.length);
 }
 
-else if (metodlar === "toString") {
-    console.log(arr.toString());
-}
-
 else if (metodlar === "join") {
     var belgi = prompt("Qaysi belgi bilan qushilsin?");
     console.log(arr.join(belgi));
 }
+else if (metodlar === "push") {
+    var item = prompt("Arrayga qushiladigan element yozing:");
+    arr.push(item);
+    console.log(arr);
+}
+
+else if (metodlar === "toString") {
+    console.log(arr.toString());
+}
+
 
 else if (metodlar === "pop") {
     arr.pop();
     console.log(arr);
 }
 
-else if (metodlar === "push") {
-    var item = prompt("Arrayga qushiladigan element yozing:");
-    arr.push(item);
-    console.log(arr);
-}
 
 else if (metodlar === "shift") {
     arr.shift();
